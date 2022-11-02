24859056852accfed0704053d7595a3d20a7859b
Xenovka <wendyanto18@gmail.com>
2022-11-02 13:57:03 +0700

    testing styled-compomnents

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  margin-top: 10px;
+  margin-left: 10px;
+`;
+
+const Home = () => {
+  return <Wrapper>Home</Wrapper>;
+};
+
+export default Home;
