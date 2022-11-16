eba622ad28835357140eaa84a627aa786a4461ec
Xenovka <wendyanto18@gmail.com>
2022-11-17 00:10:44 +0700

    improving navbar UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const activeClassName = "text-white";
+  const inactiveClassName = "text-tertiary";
+
   return (
-    <div className="p-4 w-full">
-      <div className="flex justify-around w-auto text-white font-semibold text-md">
-        <Link to="/">Home</Link>
-        <Link to="/eyes/check">Eyes Check</Link>
-        <Link to="/about">About</Link>
+    <div className="mt-10 mx-10 mb-10 w-1/4">
+      <div className="flex justify-around w-auto font-bold text-md">
+        <NavLink to="/" className={({ isActive }) => (isActive ? activeClassName : inactiveClassName)}>
+          HOME
+        </NavLink>
+        <NavLink to="/eyes/check" className={({ isActive }) => (isActive ? activeClassName : inactiveClassName)}>
+          EYES CHECK
+        </NavLink>
+        <NavLink to="/about" className={({ isActive }) => (isActive ? activeClassName : inactiveClassName)}>
+          ABOUT
+        </NavLink>
       </div>
     </div>
   );
