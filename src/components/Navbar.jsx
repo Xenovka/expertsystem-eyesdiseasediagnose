30c32c6d42b3352c204faa3c1c8590eaf8238d03
Xenovka <wendyanto18@gmail.com>
2022-11-17 18:25:16 +0700

    additional active state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Navbar = () => {
-  const activeClassName = "text-white";
-  const inactiveClassName = "text-tertiary";
+  const activeClassName = "text-secondaryLight";
+  const inactiveClassName = "text-white";
+
+  const { pathname } = useLocation();
 
   return (
     <div className="mt-10 mx-10 mb-10 w-1/4">
@@ -11,7 +13,10 @@ const Navbar = () => {
         <NavLink to="/" className={({ isActive }) => (isActive ? activeClassName : inactiveClassName)}>
           HOME
         </NavLink>
-        <NavLink to="/eyes/check" className={({ isActive }) => (isActive ? activeClassName : inactiveClassName)}>
+        <NavLink
+          to="/eyes/check"
+          className={({ isActive }) => (isActive || pathname === "/eyes/result" ? activeClassName : inactiveClassName)}
+        >
           EYES CHECK
         </NavLink>
         <NavLink to="/about" className={({ isActive }) => (isActive ? activeClassName : inactiveClassName)}>
